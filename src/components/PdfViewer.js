4c6f831797de0f0feef52c9eb597871d67272380
Imtiyaz khan <imtiyazkhan624@gmail.com>
2024-11-02 00:48:47 +0800

    MINOR BUGS FIX

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -26,18 +26,36 @@ const PDFViewer = () => {
     } else {
       console.error("Missing parameters in the URL.");
     }
+
+    // Check if the device is mobile
     setIsMobile(/Mobi|Android/i.test(navigator.userAgent));
   }, [router.query]); // Run when router.query changes
 
   return (
     <div>
-      <iframe
-        src={pdfURL}
-        width="100%"
-        height="670px"
-        title="PDF Viewer"
-        style={{ border: "none" }}
-      />
+      {isMobile ? (
+        <object
+          data={pdfURL}
+          type="application/pdf"
+          width="100%"
+          height="670px"
+          aria-label="PDF Viewer"
+          style={{ border: "none" }}
+        >
+          <p>
+            Your browser does not support PDFs.
+            <a href={pdfURL}>Download the PDF</a>.
+          </p>
+        </object>
+      ) : (
+        <iframe
+          src={pdfURL}
+          width="100%"
+          height="670px"
+          title="PDF Viewer"
+          style={{ border: "none" }}
+        />
+      )}
     </div>
   );
 };
